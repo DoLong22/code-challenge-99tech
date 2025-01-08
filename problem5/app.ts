03f66924709c34d8431af7de8f21cbd129062056
longdd <long.dd@sharitekvn.com+1-at-359181977812>
2025-01-08 21:06:51 +0700

    update env

--- problem5/app.ts
+++ problem5/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { AppDataSource } from './src/config/db';
 import userRouter from './src/services/user';
+import { errorHandler } from './src/utils/middlewares/errorHandler';
+import { NextFunction, Request, Response } from 'express';
 import { setupSwagger } from './src/docs/swagger';
 
 dotenv.config();
@@ -13,11 +15,11 @@ AppDataSource.initialize()
     .then(() => console.log('Database connected'))
     .catch(err => console.error('Database connection error:', err));
 
+// Swagger Docs
+setupSwagger(app);
 // Routes
 app.use('/api/users', userRouter);
 
-// Swagger Docs
-setupSwagger(app);
 
 app.get('/', (req, res) => {
     res.send('Welcome to User API');
--- problem5/src/config/db.ts
+++ problem5/src/config/db.ts
@@ -5,7 +5,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
   logging: false,
