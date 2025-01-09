7341a62f73e1c8381aad08e8abe6c1f0c8d43200
longdd <long.dd@sharitekvn.com+1-at-359181977812>
2025-01-09 14:29:02 +0700

    [p5]: setup auto detech route

--- problem5/server.ts
+++ problem5/server.ts
@@ -1,8 +1,54 @@
-import app from './app';
+import express from 'express';
+import dotenv from 'dotenv';
+import { AppDataSource } from './src/config/db';
+import userRouter from './src/services/user';
+import { errorHandler } from './src/utils/middlewares/errorHandler';
+import { NextFunction, Request, Response } from 'express';
+import { setupSwagger } from './src/docs/swagger';
+import { requestIdMiddleware } from './src/utils/middlewares/requestId.middleware';
 import { logger } from './src/common/logger';
+import { RequestContext } from './src/common/RequestContext';
+import path from 'path';
+import loadRouters from './src/services/route';
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
 
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+const app = express();
+app.use(express.json());
+// Swagger Docs
+setupSwagger(app);
+
+// Initialize AsyncLocalStorage context for requestId
+app.use(requestIdMiddleware);
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const requestId = RequestContext.get('requestId') || 'N/A';
+    logger.info(`${req.method} ${req.url}`);
+    next();
 });
+// Routes
+// app.use('/api/users', userRouter);
+app.use('/api', loadRouters(path.join(__dirname, 'src/services')));
+
+// Error Handler Middleware (MUST be placed after all routes)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    errorHandler(err, req, res, next);
+});
+
+export const startServer = async () => {
+    try {
+        logger.info('Connecting to the database...');
+        await AppDataSource.initialize();
+        logger.info('Database connected successfully.');
+
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            logger.info(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        logger.error('Failed to connect to the database. Server not started.');
+        logger.error(error.message);
+        // process.exit(1); // Exit the process with a failure code
+    }
+};
+
+startServer()
---
+++ problem5/src/services/route.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { logger } from '../common/logger';
+
+const loadRouters = (resourcesPath: string) => {
+    const routerMgr = express.Router();
+    const folders = fs.readdirSync(resourcesPath, { withFileTypes: true });
+    folders.forEach((folder) => {
+        if (folder.isDirectory()) {
+            const folderPath = path.join(resourcesPath, folder.name);
+            console.log('folderPath: ', folderPath);
+            const routerFiles = fs.readdirSync(folderPath).filter((file) => file.endsWith('.router.ts') || file.endsWith('.router.js'));
+            routerFiles.forEach((file) => {
+                const routerPath = path.join(folderPath, file);
+                const router = require(routerPath).default; // Assuming each router exports `default`
+                const routePrefix = `/${folder.name}s`;
+                logger.info(`Loading routes from /${folder.name}`);
+                routerMgr.use(routePrefix, router);
+            });
+        }
+    });
+    return routerMgr
+};
+export default loadRouters
